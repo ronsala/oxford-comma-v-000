9515e14df90fe4962c74e2237e5ae6867cb5775a
Ron Sala <ron.sala@gmail.com>
2017-12-14 07:21:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,13 +4,20 @@ def oxford_comma(array)
   elsif array.size == 2 
     array.join (" and ")  
   else
+    # make empty array
     last_word_ary = []
+    # set last word to var
     last_word = array.last
+    # store last word 
     last_word_ary << last_word
+    # remove last word from original array
     array.pop
-    
-    = "and"
+    # add "and" to original array
+    array << "and "
+    # convert array to string with commas
     joined = array.join(", ")
-    
+    # convert last word array to string
+    joined2 = last_word_ary.join
+    joined + joined2
   end
 end
